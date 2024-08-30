660eb731d406177145a0682e8a5d89cbbdbe341a
Tushar Parkar <parkartushar2004@gmail.com>
2024-08-30 22:21:29 +0530

    image change

--- frontend/src/components/ImageSlider.js
+++ frontend/src/components/ImageSlider.js
@@ -3,11 +3,11 @@ import Slider from 'react-slick';
 import './ImageSlider.css';
 
 // Import images
-import image1 from '../assets/images/s1.png';
-import image2 from '../assets/images/s3.png';
-import image3 from '../assets/images/s5.jpg';
-import image4 from '../assets/images/s6.png';
-import image5 from '../assets/images/s7.png';
+import image1 from '../assets/images/4.png';
+import image2 from '../assets/images/3.png';
+import image3 from '../assets/images/2.png';
+import image4 from '../assets/images/1.png';
+import image5 from '../assets/images/5.png';
 
 const images = [image1, image2, image3, image4, image5];
 
@@ -15,11 +15,11 @@ function ImageSlider() {
     const settings = {
         dots: true, // Show navigation dots
         infinite: true, // Enable continuous loop
-        speed: 400, // Transition speed in milliseconds
+        speed: 4000, // Transition speed in milliseconds
         slidesToShow: 1, // Number of slides to show at a time
         slidesToScroll: 1, // Number of slides to scroll at a time
         autoplay: true, // Enable automatic sliding
-        autoplaySpeed: 2000, // Time between slides in milliseconds
+        autoplaySpeed: 6000, // Time between slides in milliseconds
         arrows: true, // Show navigation arrows
     };
 
